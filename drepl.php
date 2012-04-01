afafa4ca2d48e56593150d33835631dc38c8cffe
Matt Chapman <Matt@NinjitsuWeb.com>
2012-04-01 12:16:11 -0700

    initial idea for Drupal REPL

---
+++ drepl.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * A PHP REPL for Drupal. Requires Drush.
+ */
+
+include('/usr/share/php/drush/includes/bootstrap.inc');
+drush_bootstrap_prepare();
+drush_bootstrap(DRUSH_BOOTSTRAP_DRUPAL_FULL);
+
