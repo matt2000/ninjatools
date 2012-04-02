b383716addfe055c6420db4caf607c11a64ba6ec
Matt Chapman <Matt@NinjitsuWeb.com>
2012-04-01 18:30:19 -0700

    I rewrote drepl and moved it to its own repo

--- drepl.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * A PHP REPL for Drupal. Requires Drush.
- */
-
-include('/usr/share/php/drush/includes/bootstrap.inc');
-drush_bootstrap_prepare();
-drush_bootstrap(DRUSH_BOOTSTRAP_DRUPAL_FULL);
-
