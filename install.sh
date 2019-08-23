63f9aa1a447635c2df33d39728dd711518520693
Matt Chapman <Matt@NinjitsuWeb.com>
2019-08-23 10:24:29 -0700

    Add git-aware-prompt

--- install.sh
+++ install.sh
@@ -2,3 +2,8 @@ ln -s ~/ninjatools/gitconfig ~/.gitconfig
 ln -s ~/ninjatools/bash_aliases ~/.bash_aliases
 ln -s ~/ninjatools/bashrc ~/.bashrc
 ln -s ~/ninjatools/vimrc ~/.vimrc
+
+mkdir ~/.bash
+cd ~/.bash
+git clone git://github.com/jimeh/git-aware-prompt.git
+cd -
